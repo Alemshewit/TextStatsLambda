2efc08511c32db2b01dc072f5713173aa23fa111
Alemshewit <alemshewitweldearegay@gmail.com>
2014-09-09 12:17:45 -0600

    TextStats works well
    More refined..

--- TextStatsLambda/TextStatsLambda/Program.cs
+++ TextStatsLambda/TextStatsLambda/Program.cs
@@ -21,19 +21,19 @@ namespace TextStatsLambda
             //take the input string argument and store in string
             string userInput = input;
             //take the string array and count each character using .Length
-            Console.WriteLine("Number of Chara " + userInput.Length);
+            Console.WriteLine("Number of Characters: " + userInput.Length);
             //select the letters in userInput where the vowels"aeiou contain the current letter in the string and count
-            Console.WriteLine("Number of vowels " + userInput.Where(x => "aeiou".Contains(x)).Count());
+            Console.WriteLine("Number of vowels: " + userInput.Where(x => "aeiou".Contains(x)).Count());
             //check to see if the current character in the string is contained in the string ",!?. /\" and count
-            Console.WriteLine("Number of special Chara " + userInput.Where(x => ",!?./ ".Contains(x)).Count());
+            Console.WriteLine("Number of special characters: " + userInput.Where(x => ",!?./ ".Contains(x)).Count());
             //select characters where the current string isn't a vowel or a character
-            Console.WriteLine("Number of consonants " + userInput.Where(x => !"aeiou.?, ".Contains(x)).Count());
+            Console.WriteLine("Number of consonants: " + userInput.Where(x => !"aeiou.?, ".Contains(x)).Count());
             //split the string by space and count the words
-            Console.WriteLine("Number of words " + userInput.Split(' ').Count());
+            Console.WriteLine("Number of words: " + userInput.Split(' ').Count());
             //split the string by space and order by length going down from the longest to the shortest and get the first word
-            Console.WriteLine("Longest Word " +userInput.Split(' ').OrderByDescending(x => x.Length).First());
+            Console.WriteLine("Longest Word: " + userInput.Split(' ').OrderByDescending(x => x.Length).First());
             //split the string by space and order by length from the shortest to the longest, and get the first word 
-            Console.WriteLine("Shortest Word " + userInput.Split(' ').OrderBy(x => x.Length).First());
+            Console.WriteLine("Shortest Word: " + userInput.Split(' ').OrderBy(x => x.Length).First());
         
         }
     }
